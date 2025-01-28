42825d77cebf53e24a63f48b45dfcdc627e158b7
Edgar Rafael Jr. <202210027@fit.edu.ph>
2025-01-28 23:28:28 +0800

    blueprint integration

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from blueprints.views.views import views
+from blueprints.routing.routing import dispose
+from blueprints.models.extract import extract
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/disposal-sites')
-def disposal_sites():
-    return render_template('disposal_sites.html')
-
-@app.route('/extract')
-def extract():
-    return render_template('extract.html')
-
-@app.route('/get-started')
-def get_started():
-    return render_template('get_started.html')
+app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(dispose, url_prefix='/')
+app.register_blueprint(extract, url_prefix='/')
 
 if __name__ == '__main__':
     app.run()
