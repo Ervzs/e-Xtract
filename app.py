4387f258ec82df315aead581a2c484c5ffec44c9
Edgar Rafael Jr. <202210027@fit.edu.ph>
2025-01-31 16:54:58 +0800

    removed unnecessary imports

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from blueprints import create_app
-from flask import Flask
-from blueprints.extract import extract_bp
-from blueprints.routing import routing_bp
-from blueprints.views import views_bp
 
 app = create_app()
 
