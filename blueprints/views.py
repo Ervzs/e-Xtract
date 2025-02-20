8a1e48356d156bca4d63c64c025bb397a2a10348
Ervin Josh Pejo <202210255@fit.edu.ph>
2025-02-20 11:55:57 +0800

    Removed redundant static_folder and template_folder from views.py

--- blueprints/views.py
+++ blueprints/views.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
 
-views_bp = Blueprint('views', __name__, static_folder='static', template_folder='templates') #initialize blueprint
+views_bp = Blueprint('views', __name__) #initialize blueprint
+
+#02.20.25 Ervzs removed static_folder and template_folder
+#create_app() in blueprints/__init__.py already sets static_folder and template_folder for the whole app.
+#No need to redefine it for each blueprint.
 
 @views_bp.route('/') 
 def home():
