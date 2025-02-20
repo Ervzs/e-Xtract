bfac4a01b636b0b3561a015b64a240e2c24db548
Ervin Josh Pejo <202210255@fit.edu.ph>
2025-02-20 11:58:33 +0800

    Removed redundant static_folder and template_folder from routing.py

--- blueprints/routing.py
+++ blueprints/routing.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
 
-routing_bp = Blueprint('routing', __name__, static_folder='static', template_folder='templates') #initialize blueprint
+routing_bp = Blueprint('routing', __name__) #initialize blueprint
+
+#02.20.25 Ervzs removed static_folder and template_folder
+#create_app() in blueprints/__init__.py already sets static_folder and template_folder for the whole app.
+#No need to redefine it for each blueprint.
 
 @routing_bp.route('/disposal-sites') 
 def disposal_route():
