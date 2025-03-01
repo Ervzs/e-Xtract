e128375a01c23c24a308612216472cec3686e7a5
benjughmin <abcbenj@proton.me>
2025-03-01 11:15:22 +0800

    Modifying HTML + CSS

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = create_app() #passes configurations to app variable
 # create_app() is defined inside blueprints/__init__.py
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
